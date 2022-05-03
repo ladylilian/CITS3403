c3b5bfdeb11ff185b702a3047cbffea1f363d565
ladylilian <f22542727@gmail.com>
2022-05-03 12:25:34 +0800

    add flask application

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+
+
+@app.route('/login')
+def login():
+    return render_template('login-page.html')
+
+@app.route('/')
+def index():
+    return render_template('game_page.html')
+
+if __name__ == "__main__":
+    app.run(debug=True) 
