0247064cdbe423d5e738ecc4f3076a2ae1e8237c
jordansylee <jordansylee@gmail.com>
2022-05-25 15:48:19 +0800

    created database
    created Players class and defined the value each column stores

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
+from crypt import methods
+from email.policy import default
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
+
+class Players(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=True, nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    max_score = db.Column(db.Integer, default=0)
 
 
 @app.route('/')
