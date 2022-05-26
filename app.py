404a4f8348a9fdd3060367f8741fffac063713f2
jordansylee <jordansylee@gmail.com>
2022-05-26 18:39:49 +0800

    Created Login page & Register page separately

--- app.py
+++ app.py
@@ -1,51 +1,87 @@
-from flask import Flask, render_template, url_for, request, redirect
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
 from datetime import datetime
+from click import confirm
+from flask import Flask, render_template, url_for, request, redirect, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, current_user
+from flask_wtf import FlaskForm
 from sqlalchemy import desc
+from wtforms import StringField,PasswordField,SubmitField,BooleanField
+from wtforms.validators import DataRequired,Email,EqualTo
+from werkzeug.security import generate_password_hash, check_password_hash
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-###app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydb.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///players.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
 
-class Players(db.Model):
+login_manger = LoginManager()
+login_manger.init_app(app)
+
+class Players(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=True, nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    username = db.Column(db.String(20), index=True, unique=True, nullable=False)
+    password_hash = db.Column(db.String(80))
     max_score = db.Column(db.Integer, default=0)
-    
+    current_score = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow, index=True)
+
+def set_password(self, password):
+    self.password_hash = generate_password_hash(password)
+
+def check_password(self, password):
+    return check_password_hash(self.password_hash, password)
+
+@login_manger.user_loader
+def load_player(player_id):
+    return Players.query.get(int(player_id))
+
+class RegistrationForm(FlaskForm):
+    username = StringField('username', validators =[DataRequired()])
+    password1 = PasswordField('Password', validators = [DataRequired()])
+    password2 = PasswordField('Confirm Password', validators = [DataRequired(),EqualTo('password1')])
+    submit = SubmitField('Register')
+
+class LoginForm(FlaskForm):
+    username = StringField('username',validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me',validators= [DataRequired()])
+    submit = SubmitField('Login')
+
+@app.route('/', methods = ['POST','GET'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_pw = generate_password_hash(form.password1.data)
+        player = Players(username =form.username.data, password_hash=hashed_pw)
+        db.session.add(player)
+        db.session.commit()
+        return redirect(url_for('login'))
+    players = Players.query.order_by(desc(Players.max_score))
+    return render_template('register_page.html', form=form, players=players)
 
 
-@app.route('/')
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        player = Players.query.filter_by(username = form.username.data).first()
+        if player is not None and check_password_hash(player.password_hash, form.password.data):
+            login_user(player)
+            next = request.args.get("next")
+            return redirect('game')
+        flash('Invalid username or password.')
+    players = Players.query.order_by(desc(Players.max_score))
+    return render_template('login_page.html', form=form, players=players)
+
+@app.route('/game')
 def index():
     players = Players.query.order_by(desc(Players.max_score))
     return render_template('game_page.html', players=players)
 
-@app.route('/login', methods=['POST','GET'])
-def login():
-    if request.method == 'POST':
-        player_uname = request.form['username']
-        player_pword = request.form['password']
-        new_player = Players(username=player_uname, password=player_pword)
-
-        try:
-            db.session.add(new_player)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue logging in'
-
-    else:
-        players = Players.query.order_by(desc(Players.max_score))
-        return render_template('login_page.html', players=players)
-
-
 if __name__ == "__main__":
     app.run(debug=True)
+
